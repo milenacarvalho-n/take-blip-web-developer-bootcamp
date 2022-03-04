e06012da08e7d8c1c693745adb4e4c210d2458c0
Milena Carvalho <nascarvalho.m@gmail.com>
2022-03-04 17:19:27 -0300

    conteúdo sobre tipos de dados

--- introducao-ao-javascript/to-do-list/assets/script.js
+++ introducao-ao-javascript/to-do-list/assets/script.js
@@ -1,2 +1,29 @@
-let form = document.getElementById('list');
-let itemList = document.getElementById('newItem')
+const form = document.getElementById("list");
+const taskList = document.getElementById("newItem");
+
+form.onsubmit = function (e) {
+  e.preventDefault();
+  const inputField = document.getElementById("listInput");
+  addTask(inputField.value);
+  form.reset();
+};
+
+function addTask(description) {
+  const taskContainer = document.createElement("div");
+  const newTask = document.createElement("input");
+  const taskLabel = document.createElement("label");
+  const taskDescriptionNode = document.createTextNode(description);
+
+  newTask.setAttribute("type", "checkbox");
+  newTask.setAttribute("name", description);
+  newTask.setAttribute("id", description);
+
+  taskLabel.setAttribute("for", description);
+  taskLabel.appendChild(taskDescriptionNode);
+
+  taskContainer.classList.add("task-item");
+  taskContainer.appendChild(newTask);
+  taskContainer.appendChild(taskLabel);
+
+  taskList.appendChild(taskContainer);
+}
---
+++ sintaxe-basica-js/variaveis e tipos/string.js
@@ -0,0 +1,24 @@
+let firstName = "Milena";
+let lastName = "Carvalho";
+
+//template string
+let fullName = `Nome completo: ${firstName} ${lastName}`;
+console.log(fullName);
+
+console.log(typeof firstName);
+
+let concatenando = firstName.concat(lastName);
+// console.log(concatenando); Output: MilenaCarvalho
+
+let concatena = firstName + " " + lastName;
+// console.log(concatenando); Output: Milena Carvalho
+concatenando.length;
+console.log(concatenando.length); // Output: 14
+
+let frase = "Olá, tudo bem?";
+console.log(frase.split(" ")); //output: [ 'Olá,', 'tudo', 'bem?' ]
+console.log(frase.split("")); // [ 'O', 'l', 'á', ',', ' ', 't', 'u', 'd', 'o', ' ', 'b', 'e',  'm', '?']
+console.log(frase.includes("tudo")); //output: true
+
+// o método abaixo retorna outra frase. Não modifica a frase original.
+console.log(frase.replace(",", "!")); // Output: "Olá! Tudo bem?"
